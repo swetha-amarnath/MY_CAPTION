42d17a2fe1a665734a9c8f14cc81bd54335fcc7e
swetha-amarnath <109688494+swetha-amarnath@users.noreply.github.com>
2022-07-20 23:21:14 +0530

    Add files via upload

---
+++ Positive_Numbers.py
@@ -0,0 +1,15 @@
+#Print all positive numbers in a range.
+
+#l1
+l1=[-1,-2,1,2,3,4,7]
+for number in l1:
+    if number >= 0:
+        print(number)
+
+
+#l2        
+l2=[-11,-1,-2,1,2,3,4,7,-9,0]
+for number in l2:
+    if number >= 0:
+        print(number)
+        
